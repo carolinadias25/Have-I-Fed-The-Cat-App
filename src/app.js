ca932c9420e50a67a5e1b7334a0038661286ca6b
carolinadias25 <carolina.f.dias@hotmail.com>
2021-05-08 15:59:17 +0100

    last upload

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 
+const { request } = require("express");
 const express = require("express");
 const { Cat } = require("./models");
 const app = express();
@@ -8,11 +9,44 @@ app.use(express.json());
 app.post("/cats", (req, res) => {
   Cat.create(req.body)
     .then((cat) => res.status(201).json(cat))
-    .catch((status, err) => {
-      console.log('err');
-      console.log(err);
-  });
+    .catch((err) => {
+      res.status(400).send(err);
+    });
 });
 
+app.get('/cats', (req, res) => {
+  Cat.findAll({where: req.query})
+    .then((cat)=> res.status(201).json(cat))
+    .catch((err) => {
+    res.status(400).send(err);
+    });
+});
+
+app.get('/cats/:catId', (req, res) => {
+  Cat.findByPk(req.params.catId)
+  .then((cat) => res.status(201).json(cat))
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
+
+app.patch('/feed/:catId', (req, res) => {
+  Cat.update(
+    { lastFed: new Date() }, 
+    { where: { id: req.params.catId }})
+  .then((cat) => res.status(201).json(cat))
+  .catch((err) => {
+      res.status(400).send(err);
+    });
+});
+
+app.delete('/cats/:catId', (req, res) => {
+  Cat.destroy(
+    { where: {id : req.params.catId}})
+  .then((catsDeleted) => res.status(202).json(catsDeleted))
+  .catch((err) => {
+    res.status(400).send(err);
+  });
+});
 
 module.exports = app;
